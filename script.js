631b6822206a1ace8eb77a47acb6c913256c42ab
Jhony Walker <60971382+JhonyWalker-pixel@users.noreply.github.com>
2020-09-07 15:36:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// Slide imagens
+
 function imgSlider(anything){
     document.querySelector('.pepsi').src = anything;
 }
+
+
+// Change background color 
 
 function changeBgChange(color) {
     const sec = document.querySelector ('.sec');
